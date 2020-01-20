6e91188d2674fd51ba416d89ecf52acaf8432310
Ubuntu <ubuntu@ip-172-31-37-46.ec2.internal>
2020-01-20 12:17:31 +0000

    building react app

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
   s3: {
     REGION: "us-east-1",
     BUCKET: "serverlesstutnotes"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://5t3uayavi4.execute-api.us-east-1.amazonaws.com/dev/"
+    URL: "https://5t3uayavi4.execute-api.us-east-1.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_H4CcKU3vV",
     APP_CLIENT_ID: "2retbs7j44cqr53gh9lqanhabr",
     IDENTITY_POOL_ID: "us-east-1:b406678f-8960-4d2b-8a7c-a16f2b0c26f8"
-  }
+  },
+  STRIPE_KEY: "pk_test_Qwngu3fxcH8DNtbekRLwyAez009ZfpQD3R"
 };
